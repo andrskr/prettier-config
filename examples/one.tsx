f4b875d0d467798f55904aa770a0c1a4c93e8c36
Andrii Skrebets <andriiskrebets@gmail.com>
2023-07-22 01:59:45 +0300

    style: format

--- examples/one.tsx
+++ examples/one.tsx
@@ -26,7 +26,7 @@ function HelloWorld({ greeting = 'hello', greeted = '"World"', silent = false, o
   let num = Math.floor(Math.random() * 1e7)
     .toString()
     .replace(/\.\d+/gi, '');
-  
+
   return (
     <div
       className="relative flex min-h-screen flex-col justify-center overflow-hidden rounded bg-blue-500 bg-gray-50 px-4 py-2 py-6 text-base text-white sm:py-12"
